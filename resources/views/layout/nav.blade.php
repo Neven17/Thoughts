f0d0c1cef101fc411cc079669687f35d2dce8634
Neven17 <vaslovikneven@gmail.com>
2024-08-20 12:43:48 +0200

    Sitna ispravljanja nav blade

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,9 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark sticky-top bg-body-tertiary" data-bs-theme="dark">
     <div class="container">
-        <a class="navbar-brand fw-light" href="/">{{ config('app.name') }}</a>
-
         <a class="navbar-brand fw-light" href="{{ route('dashboard') }}">
-            <img class="w-24" src="{{ asset('storage/logo/bijeli-oblak.png') }}" alt="Logo" style="width: 25px; height: 25px; margin-bottom: 20px;">
+            {{ config('app.name') }}
+            <img class="w-24" src="{{ asset('storage/logo/bijeli-oblak.png') }}" alt="Logo" style="width: 25px; height: 25px; margin-left: -2px; margin-bottom: 3px;  margin-top: -23px;">
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +21,7 @@
                         @php
                             $notifications = Auth::user()->notifications->take(8);
                         @endphp
-            
+
                         @forelse ($notifications as $notification)
                             <a class="dropdown-item" href="{{ route('notifications.show', $notification->id) }}">
                                 {{ $notification->data['message'] }}
@@ -30,15 +29,15 @@
                         @empty
                             <a class="dropdown-item" href="#">No new notifications</a>
                         @endforelse
-                        
+
                         <div class="dropdown-item text-center">
                             <a href="{{ route('notifications.index') }}">See All Notifications</a>
                         </div>
                     </div>
                 </li>
-            @endauth
+                @endauth
 
-               
+           
                 @guest
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">Login</a>
@@ -59,8 +58,31 @@
                             <button class="btn btn-danger btn-sm mt-1" type="submit">Logout</button>
                         </form>
                     </li>
+                    @if (Auth::user()->is_admin)
+                        <li class="nav-item">
+                            <a class="{{ Route::is('admin.dashboard') ? 'active' : '' }} nav-link"
+                                href="{{ route('admin.dashboard') }}">Admin
+                                Dashboard</a> 
+                        </li>
+                    @endif
                 @endauth
+
+                <li class="nav-item">
+                    <a class="nav-link  ms-4" href="{{ route('lang', 'en') }}">en</a> 
+                </li>
+
+
+
+                <li class="nav-item ">
+                    <a class="nav-link " href="{{ route('lang', 'hr') }}">hr</a> 
+                </li>
+
+
             </ul>
+            
+         
+
         </div>
     </div>
 </nav>
+
